7cc9b7910ea713ed2d4967efb353cd5926861808
CSY2022 <tel19807034568@outlook.com>
2023-06-20 08:57:36 +0800

    Find some mistakes and correct it.

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,11 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-  editLink: false,
-  lastUpdatedText: '最近更新',
-  contributors: false,
 })
+export default {
+  theme: defaultTheme({
+    editLink: false,
+    lastUpdatedText: '最近更新',
+    contributors: false,
+  }),
+}
