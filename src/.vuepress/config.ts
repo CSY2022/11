66ebfd49181226feb8c2d0ac9ffc56c52e64f198
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:12:31 +0800

    Another time

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,14 +2,14 @@ import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 
 export default {
-  theme: defaultTheme({
-    editLink: false,
-    lastUpdatedText: '最近更新',
-    contributors: false,
-  }),
   defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  }),
+  theme: defaultTheme({
+    editLink: false,
+    lastUpdatedText: '最近更新',
+    contributors: false,
   }),
 }
