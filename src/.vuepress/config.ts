4767e9636fba70dcf5e0fac03d2653c5019a98e5
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:09:14 +0800

    Can this work

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,17 +1,15 @@
 import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-});
-
 import { defaultTheme } from '@vuepress/theme-default'
 
-export default1 {
+export default {
   theme: defaultTheme({
     editLink: false,
     lastUpdatedText: '最近更新',
     contributors: false,
+  }),
+  defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
   }),
 }
