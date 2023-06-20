e26c4484f6f5e39e6001aeacfab32103d170139a
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:39:25 +0800

    添加主题配置。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,3 +1,5 @@
+import { defaultTheme } from 'vuepress'
+
 export default {
   locales: {
     // 键名是该语言所属的子路径
@@ -12,5 +14,14 @@ export default {
       title: 'VuePress',
       description: 'Vue 驱动的静态网站生成器',
     },
-  },
+  };
+  theme: defaultTheme({
+    // 默认主题配置
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+    ],
+  }),
 }
