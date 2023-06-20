4137188ed190e0b1a4079f4c6b5c4af9b866b73f
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:02:25 +0800

    Fixing

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,7 +4,10 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-})
+}),
+
+import { defaultTheme } from '@vuepress/theme-default'
+
 export default {
   theme: defaultTheme({
     editLink: false,
