0418dac39b204f58c23589a793d8cdd3600db097
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:36:29 +0800

    多语言支持

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,21 +1,16 @@
-import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from '@vuepress/theme-default'
-
-export default 
-locales: {
+export default {
+  locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     '/': {
-  lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
-};
-  theme: defaultTheme({
-  locales: {
-      '/': {
-  editLink: false,
-  lastUpdatedText: '最近更新',
-  contributors: false,
+      lang: 'en-US',
+      title: 'VuePress',
+      description: 'Vue-powered Static Site Generator',
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: 'VuePress',
+      description: 'Vue 驱动的静态网站生成器',
+    },
   },
-  },
-  });
+}
