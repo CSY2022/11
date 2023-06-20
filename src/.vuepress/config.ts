de52e7294e5f21488daca1108950e2fa9d44225a
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:14:00 +0800

    I won't give up.

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,13 +1,13 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 
-export default {
-  defineUserConfig({
+export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
   }),
-  theme: defaultTheme({
+{
+   theme: defaultTheme({
     editLink: false,
     lastUpdatedText: '最近更新',
     contributors: false,
