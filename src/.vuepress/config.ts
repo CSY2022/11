9c3ce0cbe4fe61ec1021ee7c86b3a269b3aa7ced
CSY2022 <tel19807034568@outlook.com>
2023-06-20 09:32:35 +0800

    All things

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,15 +1,21 @@
 import { defineUserConfig } from 'vuepress'
 import { defaultTheme } from '@vuepress/theme-default'
 
-export default {
-  defineUserConfig({
+export default 
+locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
-  });
+};
   theme: defaultTheme({
+  locales: {
+      '/': {
   editLink: false,
   lastUpdatedText: '最近更新',
   contributors: false,
+  },
+  },
   });
-}
