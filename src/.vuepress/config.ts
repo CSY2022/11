f4c694b933a9f1303960a21bcce7e73ff012bd5f
CSY2022 <tel19807034568@outlook.com>
2023-06-21 09:06:38 +0800

    vue

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,12 +1,5 @@
 import { defaultTheme } from 'vuepress'
-import { init } from 'http://unpkg.com/@waline/client@v2/dist/waline.mjs';
 
-import '@waline/client/dist/waline.css';
-
-init({
-  el: '#waline',
-  // 引入评论插件
-});
 export default {
   locales: {
     // 键名是该语言所属的子路径
@@ -17,6 +10,10 @@ export default {
       head: [
         [
         'link',{ rel: 'icon', href: 'https://csy2022.tk/11.png' }
+        ],
+        [
+        ["link", { rel: "stylesheet", href: "https://unpkg.com/@waline/client@v2/dist/waline.css" }
+        link", { rel: "stylesheet", href: "https://unpkg.com/@waline/client@v2/dist/waline-meta.css" }
         ],
       ],
       description: '婺源中学2020级11班',
