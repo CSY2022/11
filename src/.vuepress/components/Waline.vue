b65a5216fc0d07345f724a9c8a94963c465f180a
1 <2>
2023-06-25 15:56:29 +0800

    <>

---
+++ src/.vuepress/components/Waline.vue
@@ -0,0 +1,14 @@
+<template>
+  <Waline :serverURL="https://waline.csy2022.tk" :path="window.location.pathname.replace(/\/$/,'')" />
+</template>
+<script setup>
+import { Waline } from '@waline/client/component';
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+import '@waline/client/dist/waline.css';
+
+const serverURL = 'https://waline.csy2022.tk';
+const path = computed(() => useRoute().path);
+const dark = 'auto'
+</script>
