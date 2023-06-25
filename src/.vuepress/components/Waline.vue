b084ba6e1ed3906d098c3409134304e0dc8e9c05
1 <2>
2023-06-25 16:03:36 +0800

    w

--- src/.vuepress/components/Waline.vue
+++ src/.vuepress/components/Waline.vue
@@ -2,11 +2,11 @@
   <Waline :serverURL="https://waline.csy2022.tk" :path="window.location.pathname.replace(/\/$/,'')" />
 </template>
 <script setup>
-import { Waline } from '@waline/client/component';
+import { Waline } from 'https://unpkg.com/@waline/client@v2/component';
 import { computed } from 'vue';
 import { useRoute } from 'vue-router';
 
-import '@waline/client/dist/waline.css';
+import 'https://unpkg.com/@waline/client@v2/dist/waline.css';
 
 const serverURL = 'https://waline.csy2022.tk';
 const path = computed(() => useRoute().path);
