c984efa3c0687ef0d4245c9b14e32da26002bb4f
1 <2>
2023-07-12 18:10:52 +0800

    {}

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -22,8 +22,10 @@ export default {
       {
         text: '朝花夕拾',
         children: ['/guide/README.md', '/guide/teachers.md', '/guide/students.md', '/guide/bairi.md', '/guide/hechang.md', '/guide/teachersaying.md', '/guide/formorethings.md'],
+        },
+        {
         text: '留言墙',
-        link: '/guide/comments.html',
+        link: '/comments.html',
       },
     ],
     editLink: false,
