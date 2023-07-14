f0ea8c64efbe44dcd3c1d3de176524a1cdc74e94
1 <2>
2023-07-14 11:17:12 +0800

    yg

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,13 @@
 import { defaultTheme } from 'vuepress';
+import { commentPlugin } from "vuepress-plugin-comment2";
 
 export default {
+plugins: [
+    commentPlugin({
+      provider: "Waline", 
+      }),
+  ],
+};
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
