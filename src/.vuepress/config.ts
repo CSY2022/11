499f0db9296aa0212d58088128310208367803c3
1 <2>
2023-07-14 09:27:55 +0800

    shhs

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
-import { defaultTheme } from 'vuepress'
-
+import { defaultTheme } from 'vuepress';
+import { init } from '@waline/client';
+import '@waline/client/dist/waline.css';
 export default {
   locales: {
     // 键名是该语言所属的子路径
@@ -11,7 +12,10 @@ export default {
         [
         'link',{ rel: 'icon', href: '/11.png' }
         ],
-      ],
+        [
+        'link',{ rel: 'stylesheet', href: 'https://unpkg.com/@waline/client@v2/dist/waline.css'}
+        ],
+        ],
       description: '婺源中学2020级11班',
     },
   },
@@ -32,5 +36,32 @@ export default {
     lastUpdatedText: '更新日期',
     contributors: false,
     logo: '/11.png',
+      const locale = {
+      nick: '昵称（填写QQ号有惊喜）',
+      mail: '邮箱（可不填）',
+      link: '网址（可不填）',
+      admin: '站长',
+      reactionTitle: '描述一下你的心情:',
+      comment: '留言',
+      placeholder: '以手写心，畅所欲言。',
+      sofa: '来留言吧~',
+      submit: '留言',
+      login: '登录（发留言不用登录）',
+      };
+    init({
+  el: '#waline',
+  serverURL: 'https://comments.csy2022.tk',
+      search: false,
+      reaction: true, 
+      comment: true,
+      pageview: true,
+      lang: "zh-CN",
+      locale,
+      emoji: [
+      'https://unpkg.com/@waline/emojis@1.1.0/bilibili',
+      'https://unpkg.com/@waline/emojis@1.1.0/qq',
+    ],
+   
+});
   }),
 }
